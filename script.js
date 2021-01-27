fe02f9720f4826cdbf42b4e589d3fe9f379ded8f
Nishan <arifuzzamannishan@gmail.com>
2021-01-27 13:16:54 +0600

    current price for plus button fixed

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 var inputNumber;
 
 // to catch the specific element
-function getId(id){
+function getId(id) {
     return document.getElementById(id);
 }
 
 //add and minus operation
-function addSubOperation(id,sign){
+function addSubOperation(id, sign) {
     inputNumber = parseInt(document.getElementById(id).value);
-    inputNumber = inputNumber+(sign)*1;
+    inputNumber = inputNumber + (sign) * 1;
     document.getElementById
-    (id).value = inputNumber;
+        (id).value = inputNumber;
 }
 
+
+const currentPrice = getId("current-price"); //to get the current price inner text id
+const currentPriceValue = parseInt(currentPrice.innerText);
+
+
 ///plus handler
- const plusBtn = getId("plus-btn");
 
-plusBtn.addEventListener("click",function(){
-    addSubOperation("input-number-1",1); // to increase the product quantity value;
+const plusBtn = getId("plus-btn");
 
-    const currentPrice = getId("current-price"); //to get the current price inner text id
-    const currentPriceValue = parseInt(currentPrice.innerText);
-    getId("current-price").innerText = currentPriceValue*inputNumber; // to set the current price value;
+plusBtn.addEventListener("click", function () {
+    addSubOperation("input-number-1", 1);
+    getId("current-price").innerText = currentPriceValue * inputNumber; // to set the current price value;
 })
 
 // minus handler
-const minusBtn = getValueById("minus-btn");
+const minusBtn = getId("minus-btn");
 
-minusBtn.addEventListener("click",function(){
-    addSubOperation("input-number-1",-1);
+minusBtn.addEventListener("click", function () {
+    addSubOperation("input-number-1", -1);
+
+
 })
